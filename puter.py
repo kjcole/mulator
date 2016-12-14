c5370bea14a359db756cd995b67b9fd4995823ea
Kevin Cole <dc.loco@gmail.com>
2016-12-13 19:12:29 -0500

    - Added very basic boiler-plate machine instructions, etc.

--- puter.py
+++ puter.py
@@ -2,8 +2,24 @@
 # -*- coding: utf-8 -*-
 #
 #  puter.py
-#
 #  Copyright 2016 Kevin Cole <kevin.cole@novawebcoop.org> 2016.12.13
+#
+#  This is my crack at a machine language emulator.  The starting point is
+#
+#    http://appinventor.cs.trincoll.edu/csp/webapps/computer/gen0.html
+#
+#  However, we're going with an 8-bit machine, implementing it in
+#  Python and Bottle (http://bottlepy.org/), and also incorporating
+#  elements of Chris Meyer's Python 4 Fun:
+#
+#    http://www.openbookproject.net/py4fun/
+#
+#  being copied by Jeff Elkner to:
+#
+#    http://bazaar.launchpad.net/~jelkner/python4fun/python4fun/view/\
+#    head:/source/_static/mm_simulator/mm_simulator.py
+#
+#           -- GNU General Purpose License (GPL) --
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License as
@@ -42,6 +58,43 @@ __status__     = "Prototype"  # "Prototype", "Development" or "Production"
 __module__     = ""
 
 
+class CPU:
+
+    def __init__(self):
+        """Build a machine"""
+        self.instruction_register = 0
+        self.accumulator = 0
+        self.instruction_counter = 0
+        self.monitor = None
+        self.ram = [0] * 8192  # This has a whopping 8 KB of RAM !!!
+        self.instruction_set = {0b1000: self.reboot,
+                                0b1001: self.add,
+                                0x1010: self.store,
+                                0x1011: self.load,
+                                0x1100: self.prn}
+
+    def reboot(self):
+        """Reinitialize everything on a reboot"""
+        self.__init__()
+
+    def add(self, address):
+        """Add value at address to value in accumulator"""
+        self.accumulator += self.ram[address]
+
+    def store(self, address):
+        """Store value from accumulator into memory address"""
+        self.ram[address] = self.accumulator
+
+    def load(self, address):
+        """Load value from memory address into accumulator"""
+        self.accumulator = self.ram[address]
+
+    def prn(self, address):
+        """Print value at memory address to monitor"""
+        self.monitor = self.ram[address]
+        print(self.monitor)  # Use bottle, not print to stdout
+
+
 def main():
     _ = os.system("clear")
     print("{0} v.{1}, {2} <{3}> ({4})\n"
@@ -51,7 +104,8 @@ def main():
                   __email__,
                   __license__))
 
-    # [Your code here]
+    cpu = CPU()
+    cpu.instruction_set[0x1010](0b1010)  # Print what's at location 1010
 
     return 0
 
